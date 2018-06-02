472e1035446602ac48da638730e323ffae7b2f27
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-06-02 18:10:17 +0200

    Added html and js files

--- README.md
+++ README.md
@@ -2,13 +2,49 @@
 
 # API Specification
 
-https://app.swaggerhub.com/apis/RJK-Gorilla/PopupBox
+https://app.swaggerhub.com/apis/RJK-Gorilla/Messages
+
+C - POST /message - message = request.data, return /message/{id}
+R - GET /message
+R - GET /message/{id}
+U - PUT /message/{id} - updateMessage(request.data)
+D - DELETE /message/{id}
+
+# Hypermedia links
+
+Hypertext (ht\*) is hypermedia. Hypermedia contains hyperlinks.
+
+rel = relation !
+
+``
+``
+
+# Annotations
+
+Annotation  | Import
+----------- | ------
+@GET        | import javax.ws.rs.GET;
+@Produces   | import javax.ws.rs.Produces;
+@Path       | import javax.ws.rs.Path;
+@PathParam  | import javax.ws.rs.PathParam;
+@QueryParam | import javax.ws.rs.QueryParam;
+@POST       | import javax.ws.rs.POST;
+@Consumes   | import javax.ws.rs.Consumes;
+@FormParam  | import javax.ws.rs.FormParam;
+@PUT        | import javax.ws.rs.PUT;
+@DELETE     | import javax.ws.rs.DELETE;
 
 # References
 
 * https://en.wikipedia.org/wiki/Java_API_for_RESTful_Web_Services
 * Richardson Maturity Model - https://www.martinfowler.com/articles/richardsonMaturityModel.html
+* Diff between POST/PUT
+    * https://zacharyvoase.com/2009/07/03/http-post-put-diff
+    * http://restcookbook.com/HTTP%20Methods/put-vs-post
 * http://www.enterpriseintegrationpatterns.com/patterns/messaging/EncapsulatedSynchronousIntegration.html
+* http://www.javawebtutor.com/articles/web-services/rest/creating-web-service-with-maven-jersey.php
+* http://www.docjar.com/docs/api/org/json/JSONObject.html#put(String,%20Collection)
+* https://jersey.github.io/documentation/latest/getting-started.html
 
 # Eclipse
 
---
+++ WebContent/rjk/PopupBox.js
@@ -0,0 +1,185 @@
+define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+
+    "dojo/_base/array",
+    "dojo/_base/fx",
+    "dojo/_base/lang",
+    "dojo/_base/window",
+    "dojo/dom-attr",
+    "dojo/dom-construct",
+    "dojo/dom-style",
+    "dojo/mouse",
+    "dojo/on",
+
+    "dojo/store/Memory",
+    "dojo/store/JsonRest",
+    "dojo/store/Observable"
+], function (declare, _WidgetBase, _TemplatedMixin,
+    array, bfx, lang, win, attr, constr, style, mouse, on,
+    Memory, JsonRest, Observable) {
+
+    declare("rjk.PopupBox", [_WidgetBase, _TemplatedMixin], {
+        timeout: 5000,
+        showDuration: 500,
+        hideDuration: 200,
+        hideMessageDuration: 200,
+
+        width: 400,
+        height: 200,
+        margin: 10,
+        border: "1px solid black",
+        backgroundColor: "#fff",
+        iconSize: 20,
+        iconColor: "#73ad21",
+
+        messageContainerNode: null,
+        iconNode: null,
+        hideTimer: null,
+
+        templateString: '<div><div class="rjk_PopupBox_messageContainer"></div></div>',
+
+        store: null,
+
+        buildRendering: function(){
+            this.inherited(arguments);
+            this._setupIconNode();
+            this._setupPopupNode();
+        },
+
+        postCreate: function() {
+            this.store = new JsonRest({
+                target: "/RSApi/api/messages"
+                // target: "http://localhost:8080/RSApi/api/messages"
+            });
+            this.store = new Observable(this.store);
+            this.store.query().observe(
+                lang.hitch(this, function (message, removedFrom, insertedInto) {
+                    if (removedFrom == -1)
+                        this.addMessage(message);
+                })
+            );
+            window.store = this.store;
+
+            this.loadMessages();
+        },
+
+        loadMessages: function() {
+            this.store.query().then(lang.hitch(this, function(list){
+                array.forEach(list, lang.hitch(this, function (message) {
+                    this.addMessage(message);
+                }))
+            }));
+            this.refeshIcon();
+        },
+
+        _setupIconNode() {
+            var size = this.iconSize * 2;
+            this.iconNode = constr.create("div", {
+                style: {
+                    display: "none",
+                    position: "absolute",
+                    width: size + "px",
+                    height: size + "px",
+                    right: this.margin + "px",
+                    bottom: this.margin + "px",
+                    'border-radius': this.iconSize + "px",
+                    background: this.iconColor,
+                    'z-index': 998
+                }
+            }, win.body(), "first");
+
+            on(this.iconNode, mouse.enter, lang.hitch(this, "show"));
+        },
+
+        _setupPopupNode: function() {
+            style.set(this.domNode, {
+                position: "absolute",
+                overflow: "hidden",
+                width: this.width + "px",
+                right: this.margin + "px",
+                bottom: 0,
+                opacity: 0,
+                'background-color': this.backgroundColor,
+                'z-index': 999
+            });
+            this.messageContainerNode = this.domNode.children[0];
+            style.set(this.messageContainerNode, {
+                border: this.border,
+                height: "100%"
+            });
+            constr.place(this.domNode, win.body(), "first");
+
+            on(this.domNode, mouse.leave, lang.hitch(this, "hide"));
+        },
+
+        message: function (text) {
+            this.store.put(
+                { "text":text }
+            );
+        },
+
+        addMessage: function (message) {
+            var node = constr.toDom(
+                '<div class="rjk_PopupBox_message" style="overflow: hidden">' +
+                message.text + "<hr></div>"
+            );
+            attr.set(node, 'data-id', message.id);
+            constr.place(node, this.messageContainerNode, "first");
+
+            on(node, "click", lang.hitch(this, function (e) {
+                bfx.animateProperty({
+                    node: node,
+                    postMixInProperties: { height: 0 },
+                    duration: this.hideMessageDuration,
+                    onEnd: lang.hitch(this, function () {
+                        if (node.parentNode) { // can be null on fast clicking
+                            node.parentNode.removeChild(node);
+                            this.store.remove(attr.get(node, 'data-id'));
+                            this.refeshIcon();
+                        }
+                    })
+                }).play();
+            }));
+
+            this.refeshIcon();
+            this.show(this.timeout);
+        },
+
+        show: function (timeout) {
+            clearTimeout(this.hideTimer);
+
+            bfx.animateProperty({
+                node: this.domNode,
+                properties: {
+                    height: this.height,
+                    opacity: 1
+                },
+                duration: this.showDuration
+            }).play();
+
+            if (timeout != null)
+                this.hideTimer = setTimeout(lang.hitch(this, function () {
+                    this.hide();
+                }), timeout + this.showDuration);
+        },
+
+        hide: function () {
+            bfx.animateProperty({
+                node: this.domNode,
+                properties: {
+                    height: 0,
+                    opacity: 0
+                },
+                duration: this.hideDuration
+            }).play();
+        },
+
+        refeshIcon() {
+            style.set(this.iconNode, {
+                display: this.messageContainerNode.children.length == 0 ? "none" : "inline-block"
+            });
+        }
+    });
+});
