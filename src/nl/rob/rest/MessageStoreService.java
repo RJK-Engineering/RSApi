aad5163abee1a6c8a272d2fe9d1d8cac2c173d81
Rob Klinkhamer <rjklinkhamer@gmail.com>
2018-06-02 18:13:37 +0200

    Completed service

--- src/nl/rob/rest/MessageStoreService.java
+++ src/nl/rob/rest/MessageStoreService.java
@@ -1,9 +1,9 @@
 package nl.rob.rest;
 
-import java.io.InputStream;
-
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,42 +11,78 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 @Path("/messages")
 public class MessageStoreService {
 
+	@GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIndex() {
+		MessageStore ms = getMessageStore();
+        JSONArray data = new JSONArray();
+        for (Message m : ms.getMessages()) {
+        	data.put(toJSON(m));
+        }
+        return Response.status(200).entity(data.toString()).build();
+    }
+
+	@GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMessage(@PathParam("id") String idParam) {
+		MessageStore ms = getMessageStore();
+		int id = Integer.parseInt(idParam);
+		Message m = ms.getMessage(id);
+        return Response.status(200).entity(toJSON(m).toString()).build();
+    }
+
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	public Response createMessage(String body) {
+    	JSONObject data = new JSONObject(body);
+		MessageStore ms = getMessageStore();
+		Message m = ms.createMessage(data.getString("text"));
+		return Response.status(200).entity(toJSON(m).toString()).build();
+    }
+
     @PUT
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getMsg(InputStream data) {
-		return null;
-    }
-
-    @GET
-    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getMsg(@PathParam("id") String idParam) {
-
-        JSONObject json = new JSONObject()
-            .put("firstName", "John")
-            .put("lastName", "Smith")
-            .put("age", 25)
-            .put("address", new JSONObject()
-                .put("streetAddress", "21 2nd Street")
-                .put("city", "New York")
-                .put("state", "NY")
-                .put("postalCode", "10021"))
-            .append("phoneNumber", new JSONObject()
-                .put("type", "home")
-                .put("number", "212 555-1234"))
-            .append("phoneNumber", new JSONObject()
-                .put("type", "fax")
-                .put("number", "646 555-4567"));
-//        int id = Integer.parseInt(idParam);
-
-        return Response.status(200).entity(json.toString()).build();
-
+    public Response updateMsg(String body) {    	
+    	JSONObject data = new JSONObject(body);
+		MessageStore ms = getMessageStore();
+		Message m = ms.getMessage(data.getInt("id"));
+		m.text = data.getString("text");
+        return Response.status(200).entity(toJSON(m).toString()).build();
     }
+
+	@DELETE
+    @Path("/{id}")
+    public Response deleteMessage(@PathParam("id") String idParam) {
+		MessageStore ms = getMessageStore();
+		int id = Integer.parseInt(idParam);
+		ms.deleteMessage(id);
+        return Response.status(200).build();
+    }
+
+    private MessageStore getMessageStore() {
+		MessageStore ms = new MessageStore();
+		return ms;
+	}
+
+	private static JSONObject toJSON(Message m) {
+		JSONObject o = new JSONObject();
+		if (m != null) {
+			o.put("id", m.id);
+			o.put("text", m.text);
+		}
+		return o;
+	}
 
 }
